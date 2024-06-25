d18c21758b55aa98d05c5374dec46ec45ed6ebaa
Ved Tellawar <tellawarved@gmail.com>
2024-06-25 18:28:01 +0530

    Created pages and Routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,30 @@
+import { lazy, Suspense } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Loader from './components/Loader';
 
+const Home = lazy(() => import('./pages/Home'));
+const SignUp = lazy(() => import('./pages/SignUp'));
+const SignIn = lazy(() => import('./pages/SignIn'));
+const About = lazy(() => import('./pages/About'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const Projects = lazy(() => import('./pages/Projects'));
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Suspense fallback={<Loader/>}>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/signup" element={<SignUp/>} />
+            <Route path="/signin" element={<SignIn/>} />
+            <Route path="/projects" element={<Projects/>} />
+            <Route path="/dashboard" element={<Dashboard/>} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </div>
   )
 }
 
