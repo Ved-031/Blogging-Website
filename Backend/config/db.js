364550649240c34f0a0486511413befcba9d63f9
Ved Tellawar <tellawarved@gmail.com>
2024-06-26 12:32:07 +0530

    Connect DB

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose'
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO)
+    await mongoose.connect(process.env.MONGO_URI)
     .then(()=>{
-        console.log("db connected");
+        console.log("db connected")
     })
     .catch((err)=>{
-        console.log(err);
+        console.log(err)
     })
 }
 
